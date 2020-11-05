af851791dcae217295addb750e34efc83253763f
JHIH-SIOU LI <j.s.li@gapp.nthu.edu.tw>
2020-11-05 12:30:08 +0800

    update

---
+++ a082.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// class to manage card stack
+class Card_Stack {
+	private:
+	// class to store basic card properties
+	class Card {
+		private:
+		char ch_type;
+		int int_type (char card_type) {
+			switch (card_type) {
+				case 'S':
+					return 4;
+				case 'H':
+					return 3;
+				case 'D':
+					return 2;
+				case 'C':
+					return 1;
+			}
+			return 0;
+		}
+	
+		public:
+		int type;
+		int number;
+		Card (char card_type, int card_number) {
+			number = card_number;
+			type = int_type (card_type);
+			ch_type = card_type;
+		}
+		void print () {
+			cout << ch_type << number << " ";
+		}
+	};
+	static bool cmp (Card X, Card Y) {
+		if (X.type != Y.type)
+			return X.type < Y.type;
+		else
+			return X.number < Y.number;
+	}
+	
+	public:
+	vector<Card> card_list;
+
+	Card_Stack () {
+		string tmp;
+		getline (cin, tmp);
+		
+		size_t position = 0;
+		
+		string token, delim = " ";
+		while ((position = tmp.find (delim)) != string::npos) {
+			token = tmp.substr (0, position);
+			Card card (token[0], stoi (token.substr (1, token.length() - 1)));
+			card_list.push_back (card);
+			tmp.erase (0, position + delim.length ());
+		}
+	}
+	
+	void qsort () {
+		sort (card_list.begin (), card_list.end (), cmp);
+	}
+
+	void print () {
+		for (int i = 0; i < card_list.size (); i++) 
+			card_list[i].print ();
+		cout << endl;
+	}
+
+};
+
+
+// driver code
+int main ()
+{	
+	int stack_count;
+	cin >> stack_count;
+	for (int i = 0; i < stack_count; i++) {
+		Card_Stack card_stack;
+		card_stack.qsort ();
+		card_stack.print ();
+	}	
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+build:
+	gcc a082.cpp -o a082.o
+	./a082.o < a082.input
